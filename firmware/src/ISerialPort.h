befc546abc0bb0c6c56e102484ec8ecf0c8382e2
Lech <lech.perczak@gmail.com>
2017-07-16 22:01:27 +0200

    Added serial port interface declaration.

---
+++ firmware/src/ISerialPort.h
@@ -0,0 +1,38 @@
+#ifndef ISERIALPORT_H
+#define ISERIALPORT_H
+
+#include <stddef.h>
+
+namespace hswro {
+namespace elektrozamek {
+
+class ISerialPort
+{
+public:
+    enum Parity
+    {
+        None,
+        Odd,
+        Even
+    };
+
+    enum StopBits
+    {
+        One,
+        OneAndHalf,
+        Two
+    };
+
+    virtual ~ISerialPort {};
+    virtual size_t read(char* output, size_t size) = 0;
+    virtual size_t write(char* output, size_t size) = 0;
+    virtual void flush() = 0;
+
+    virtual void setBaud(int baud) = 0;
+    virtual void setParity(Parity parity) = 0;
+    virtual void setStopBits(StopBits stopBits) = 0;
+};
+
+}}
+
+#endif // ISERIALPORT_H
